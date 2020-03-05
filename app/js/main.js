c796e90863d145f1652afd5a9b74ff0a4589eb7f
JuliaShybunka <julishybunka@gmail.com>
2020-03-05 15:40:32 +0200

    Style shop slider

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,21 @@ $(document).ready(function() {
     arrows: false
   });
 
-  $(".shop__items").slick({
-    dots: true,
-    speed: 300,
+  var slickOpts = {
     slidesToShow: 4,
     slidesToScroll: 4,
-    arrows: false
-  });
+    //centerMode: true,
+    easing: "swing", // see http://api.jquery.com/animate/
+    speed: 700,
+    arrows: true,
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-prev">Previous</button></div>',
+
+    customPaging: function(slick, index) {
+      return "<a>" + (index + 1) + "</a>";
+    }
+  };
+  // Init slick carousel
+  $(".shop__items").slick(slickOpts);
 });
