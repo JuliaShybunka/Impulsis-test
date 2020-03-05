56d8aa188a3415ddc2b2b8aa588f1821af46642f
JuliaShybunka <julishybunka@gmail.com>
2020-03-05 09:29:32 +0200

    Create about section and footer, and add style

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
     dots: true,
     arrows: false
   });
+
+  $(".shop__items").slick({
+    dots: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false
+  });
 });
